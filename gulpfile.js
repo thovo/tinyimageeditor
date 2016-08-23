98fc1ce5316169c9649734e7ef4c7d3b76f1c239
DANG Thai Lam <lam.dang@businessline.fr>
2016-08-23 23:14:32 +0200

    Commit gulp-pug

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,22 @@ var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
+var pug = require('gulp-pug');
 
-gulp.task('default', ['build', 'sass', 'connect'], function() {
+gulp.task('default', ['build', 'sass', 'views', 'connect'], function() {
     gulp.watch(['./src/scripts/**/*.js',
-        './src/style/**/*.scss'
-    ], ['build', 'sass']);
+        './src/style/**/*.scss',
+        './src/views/**/*.pug'
+    ], ['build', 'sass', 'views']);
 
+});
+
+gulp.task('views', function() {
+  return gulp.src('./src/views/*.pug')
+  .pipe(pug({
+    // Your options in here.
+  }))
+  .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('build', function() {
