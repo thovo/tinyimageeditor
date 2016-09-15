49879da07a9eb9000ee24000310b02301544c816
Tuong Tho VO <votuongtho@gmail.com>
2016-09-15 13:42:27 +0200

    Update prototype with new 3 filters

--- src/scripts/imageHandler.js
+++ src/scripts/imageHandler.js
@@ -4,13 +4,86 @@
   var context = canvas.getContext('2d');
   var uploadButton = document.getElementById('btn-upload');
   var downloadButton = document.getElementById('btn-download');
+  var undoButton = document.getElementById('btn-undo');
   var inputUpload = document.getElementById('input-upload');
-  var image = new Image();
+  var filterButtons = document.getElementsByClassName('filter');
+  var undoData = {};
 
   function addEventsListener() {
     uploadButton.addEventListener('click', toggleOpenFileDialog, false);
     inputUpload.addEventListener('change', uploadImage, false);
     downloadButton.addEventListener('click', downloadImage, false);
+    undoButton.addEventListener('click', undoChange, false);
+    for (var i = 0; i < filterButtons.length; i++) {
+      filterButtons[i].addEventListener('click', filters, false);
+    }
+  }
+
+  function filters(e) {
+    var filter = e.target.id;
+    var imageData = {};
+    if (canvas.width > 0 && canvas.height > 0) {
+      imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      undoData = context.getImageData(0, 0, canvas.width, canvas.height);
+    }
+    switch (filter) {
+      case 'grayscale':
+        grayScale(imageData);
+        break;
+      case 'sepia':
+        sepia(imageData);
+        break;
+      case 'brightness':
+        brightness(imageData);
+        break;
+      default:
+
+    }
+  }
+
+  function grayScale(imageData) {
+    var d = imageData.data;
+    var len = d.length;
+    for (var i = 0; i < len; i += 4) {
+      var r = d[i];
+      var g = d[i + 1];
+      var b = d[i + 2];
+      d[i] = d[i + 1] = d[i + 2] = (r+g+b)/3;
+    }
+    updateCanvas(imageData);
+  }
+
+  function sepia(imageData) {
+    var d = imageData.data;
+    var len = d.length;
+    for (var i = 0; i < len; i += 4) {
+      var r = d[i];
+      var g = d[i + 1];
+      var b = d[i + 2];
+      d[i]     = (r * 0.393)+(g * 0.769)+(b * 0.189); // red
+      d[i + 1] = (r * 0.349)+(g * 0.686)+(b * 0.168); // green
+      d[i + 2] = (r * 0.272)+(g * 0.534)+(b * 0.131); // blue
+    }
+    updateCanvas(imageData);
+  }
+
+  function brightness(imageData) {
+    var d = imageData.data;
+    var len = d.length;
+    for (var i = 0; i < len; i += 4) {
+          d[i] += 50;     // red
+          d[i + 1] += 50; // green
+          d[i + 2] += 50; // blue
+        }
+    updateCanvas(imageData);
+  }
+
+  function undoChange() {
+    updateCanvas(undoData);
+  }
+
+  function updateCanvas(imageData) {
+    context.putImageData(imageData, 0, 0);
   }
 
   function toggleOpenFileDialog() {
