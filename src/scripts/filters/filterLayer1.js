291d57c6413ee860d095fc8ced92ae3950fdf342
Kihansi95 <ndh3395@gmail.com>
2016-09-17 16:10:57 +0200

    wrap canvas to uploaded image

--- src/scripts/filters/filterLayer1.js
+++ src/scripts/filters/filterLayer1.js
@@ -9,11 +9,11 @@ function filterLayer1() {
     //get back my image from canvas (a little redundancy)
     var image = new Image();
     image.src = canvas.toDataURL();
-    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    context.drawImage(image, 0, 0, image.width, image.height);
     context.globalCompositeOperation = "soft-light";    //hoặc mình hòa nhập filter mạnh hơn bằng value "lighter"
 
     //I paint my context by rainbow
-    var gradient = context.createLinearGradient(0, canvas.height, canvas.width, canvas.height);
+    var gradient = context.createLinearGradient(0, image.height, image.width, image.height);
 
     gradient.addColorStop(0.000, 'rgba(255, 0, 0, 1.000)');
     gradient.addColorStop(0.333, 'rgba(225, 255, 0, 1.000)');
@@ -21,5 +21,5 @@ function filterLayer1() {
     gradient.addColorStop(1.000, 'rgba(0, 55, 255, 1.000)');
 
     context.fillStyle = gradient;
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillRect(0, 0, image.width, image.height);
 }
